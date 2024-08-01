97b01cce37691bd6104032b7d657f57236883952
husnain.raza <husnain.raza@genetechsolutions.com>
2024-08-01 19:32:45 +0500

    add search functionality in api

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,18 @@
 const Product = require('../models/product')
 
 const getAllProducts = async (req,res) => {
-    const items = await Product.find({});
+
+    const{company,name} = req.query;
+    const queryObject = {};
+
+if (company) {
+    queryObject.company = {$regex: company};
+}
+if (name) {
+    queryObject.name = {$regex: name};
+}
+
+    const items = await Product.find(queryObject);
     res.status(200).json({items})
 }
 
